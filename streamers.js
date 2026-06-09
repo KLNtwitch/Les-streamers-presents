35090cc228b959cfc26254dad4cfde162be0a235
KLNtwitch <kyllianhp@gmail.com>
2026-06-09 14:55:25 +0200

    Update streamers.js

--- streamers.js
+++ streamers.js
@@ -1,11 +1,17 @@
 const streamers = [
+
 {
 name:"KLNtwitch",
-twitch:"https://twitch.tv/KLNtwitch"
+avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png",
+twitch:"https://twitch.tv/KLNtwitch",
+live:true
 },
 
 {
 name:"e2manuel_yt",
-twitch:"https://twitch.tv/e2manuel_yt"
+avatar:"https://static-cdn.jtvnw.net/jtv_user_pictures/xarth/404_user_70x70.png",
+twitch:"https://twitch.tv/e2manuel_yt",
+live:false
 }
+
 ];
